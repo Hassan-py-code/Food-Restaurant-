0c0d9a22dbb6f425e7bd4ae94eed31298e1e17cf
Hassan Achehri <havhassanb@gmail.com>
2025-08-09 01:11:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+
+// function roundOff(num, ndigits) {
+//      return  num.toFixed(ndigits) ;  
+// };
+
+
+
+// console.log(roundOff(0.778899,2));
+
+// function calculateSum(num1, num2) {
+
+//     for(let i=num1; i< num2+1;i++ ){
+//      let result=i;
+//     //  let list=[result];
+//     // //  list.push(result)
+//     //  console.log(list)
+//      console.log(result);
+//     };
+    
+// } 
+
+// console.log(calculateSum(10,15));
+
