d235312abbd1be4f7cae9a283cc2f13c030a66c3
Hassan Achehri <havhassanb@gmail.com>
2025-08-09 19:15:16 +0200

    add service and footer and creat js

--- main.js
+++ main.js
@@ -24,3 +24,90 @@
 
 // console.log(calculateSum(10,15));
 
+
+
+let headerMenuToggle=document.querySelector(".header_menus-toggle");
+let headerMenuss=document.querySelector(".header_menus ");
+
+
+headerMenuToggle.addEventListener("click",()=>{
+     headerMenuss.classList.toggle('active');
+});
+
+
+
+ ScrollReveal(
+    { reset: true,
+        delay:300,
+        distance:"60px",
+        duration:1500,
+
+     }
+);
+
+// components
+ScrollReveal().reveal('.section-title');
+ScrollReveal().reveal('.section-des',{
+     delay:300,
+     duration:2000
+});
+
+
+
+
+// section Home
+ScrollReveal().reveal('.section-home_title');
+ScrollReveal().reveal('.section-home-image',{
+    origin:"right"
+});
+
+ScrollReveal().reveal('.section-home-buttons',{
+     delay:600
+});
+
+// ScrollReveal().reveal('.section-home_title',{
+//     origin:"left"
+// });
+
+
+// section Menus
+ScrollReveal().reveal('.section-menus_card',{
+     interval:300,
+});
+
+// section About
+ScrollReveal().reveal('.section-about_image"',{
+     delay:900,
+     origin:"left"
+});
+
+
+ScrollReveal().reveal('.section-about_stat',{
+     delay:1200,
+     interval:300
+});
+
+ScrollReveal().reveal(".section-about  .primary-button",{
+       duration:3000,
+       delay:1200
+});
+
+
+// section services
+
+ScrollReveal().reveal(".section-servecs_card",{
+     delay:1200,
+     interval:300
+});
+
+// section contct
+
+ScrollReveal().reveal(".section-contact-item",{
+     delay:1200,
+     interval:300
+})
+
+ScrollReveal().reveal('.section-contact__from',{
+     delay:1800,
+     duration:3000
+})
